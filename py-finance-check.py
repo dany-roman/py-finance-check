ae7281a7df0027e7565f817b5b8aa3ced65e27e4
unknown <dany.roman.phd@gmail.com>
2021-04-30 19:06:47 -0500

    Created summary table, total months, and text for outputs

--- py-finance-check.py
+++ py-finance-check.py
@@ -4,6 +4,23 @@ df = pd.read_csv (r'C:\Users\Doc\Repositories\py-finance-check\Test Data\budget_
 
 print (df)
 
-df.count(index[0]).count()
+nmonth = df['Date']
+nettotal = df['Profit/Losses'].sum()
+grincrease = df['Profit/Losses'].max()
+grdecrease = df['Profit/Losses'].min()
+
+# df.insert(2, "MoM Change", "")
+
+# df['MoM Change'] = df.Profit/Losses.diff()
+# print(df)
 
 
+
+print("------------------------------------------")
+print("Financial Analysis")
+print("------------------------------------------")
+print(f'Total Months: {len(nmonth)}')
+print(f'Net Total: ${nettotal}')
+#print(f'Avg. Change: ${avgchange}')
+print(f'Greatest Increase in Profit: ${grincrease}')
+print(f'Greatest Decrease in Profit: ${grdecrease}')
