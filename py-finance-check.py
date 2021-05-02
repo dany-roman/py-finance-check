a0503ad90b6736ec8fc1ad3251519f53b3451ae0
unknown <dany.roman.phd@gmail.com>
2021-05-02 18:49:21 -0500

    finance check code is working properly

--- py-finance-check.py
+++ py-finance-check.py
@@ -15,50 +15,29 @@ with open(csvpath) as csvfile:
     csv_header = next(csvreader)
     print(f"CSV Header: {csv_header}")
 
-    print("------------------------------------------")
-    print("Test")
-    print("------------------------------------------")
-    
     prev_row = 0
+    delta = 0
+
     for row in csvreader:
-        months.append(row[0])
-        profloss.append(row[1])
+        months.append(str(row[0]))
+        profloss.append(int(row[1]))  
         delta = int(row[1]) - prev_row
         momchange.append(delta)
         prev_row = int(row[1])
-        
-    print("------------------------------------------") 
-    print("Months")
-    print("------------------------------------------")
-    print(months)
-    print("------------------------------------------")
-    print("Mom")
-    print("------------------------------------------")
-    print(momchange)
-
-    # months = [months.append(row[0]) for row in csvreader]
-
-    # print(months)
-
-    # nmonth = [len(nmonth.append(row[0]) for row[0] in csvreader)]
-
-    # print(nmonth)
-# nettotal = sum(row[1] for row in csv_header)
-# grincrease = csvreader['delta'].max()
-# grdecrease = csvreader['delta'].min()
-
-# prev_row = 0
-
-# for row in csvreader:
-#     delta = row[2] - prev_row
-#     csvreader.append(delta)
-#     prev_row = row[0]
+    
+    momchange[0] = 0
+    nmonth = len(months)
+    nettotal = sum(profloss)
+    avgchange = round((sum(momchange) / nmonth), 2)
+    maxchange = max(momchange)
+    minchange = min(momchange)
 
 print("------------------------------------------")
 print("Financial Analysis")
 print("------------------------------------------")
-# print(f'Total Months: {nmonth}')
-# print(f'Net Total: ${nettotal}')
-#print(f'Avg. Change: ${avgchange}')
-# print(f'Greatest Increase in Profit: ${grincrease}')
-# print(f'Greatest Decrease in Profit: ${grdecrease}')
+print(f'Total Months: {nmonth}')
+print(f'Net Total: ${nettotal}')
+print(f'Avg. Change: ${avgchange}')
+print(f'Greatest Increase in Profit: ${maxchange}')
+print(f'Greatest Decrease in Profit: ${minchange}')
+print("------------------------------------------")
